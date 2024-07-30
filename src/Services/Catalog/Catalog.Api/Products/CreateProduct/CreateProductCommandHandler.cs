03bf7cb96bfdf657da1c616a5be16db0f94faa76
PhongT <ttphong.dh15pm@gmail.com>
2024-07-31 00:11:34 +0700

    Day 1: - Create Abstraction on MediatR for CQRS - Implement Carter with Minimal API - Install Mapster for model mapping

---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using BuildingBlocks.CQRS;
+using MediatR;
+
+namespace Catalog.Api.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
